8753123fa4d33c0264d128acb9795fa946870552
potatoes-and-molasses <eedoshapira@gmail.com>
2017-12-03 13:06:58 +0200

    wmi subscriptions test1

---
+++ wmi_subs_test.ps1
@@ -0,0 +1,5 @@
+﻿$query = "select * from __instanceoperationevent within 2 where targetinstance isa 'CIM_DataFile' and targetinstance.drive='C:' and targetinstance.path='\\wmitest\\'"
+Register-WmiEvent -Query $query -Action {Write-Host 1;$EventArgs.NewEvent.TargetInstance.name;[io.file]::WriteAllText('C:\users\MY_USER_HEHE\desktop\resultserialize',[System.Management.Automation.PSSerializer]::Serialize($EventArgs))} -MessageData $obj -SourceIdentifier 'mytest'
+
+#Unregister-Event -SourceIdentifier mytest
+#$b = [System.Management.Automation.PSSerializer]::Deserialize([io.file]::ReadAllText('C:\users\MY_USER_HEHE\desktop\resultserialize'))
