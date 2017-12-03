b617c871c80acfdb391190b879a6420cc271663f
potatoes-and-molasses <eedoshapira@gmail.com>
2017-12-03 13:00:58 +0200

    searching for strings in files with nice powershelly output

---
+++ search_func.ps1
@@ -0,0 +1,6 @@
+﻿function findmatches($folder,$regex){
+"searching for `"$regex`" in $folder..."
+gci $folder -Recurse | %{$filename=$_.fullname;gc $filename -ea si | %{if($_ -match $regex){$filename}} | group | select name, count}
+}
+
+#findmatches 'C:\users\public' 'trollololol'
