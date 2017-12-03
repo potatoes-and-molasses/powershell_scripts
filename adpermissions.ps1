c79b9bb62c2eb62301b16568e74d261c92e7cdcb
potatoes-and-molasses <eedoshapira@gmail.com>
2017-12-03 12:48:50 +0200

    enumerating permissions on AD objects with dsacls, nice powershell formatting

---
+++ adpermissions.ps1
@@ -0,0 +1,53 @@
+﻿
+
+function parseacls($dn){
+#for example: parseacls "OU=Apps,DC=LOL"
+#returns permissions on an AD object in a nice powershell format
+#kinda slow because dsacls.exe is slow... but what can you do:(
+$badformat = (dsacls.exe $dn)
+foreach($i in 1..$badformat.length){
+    $obj = New-Object psobject -Property @{'Type'='';'Group'='';'Permissions'=''}
+    if($badformat[$i] -match 'Permissions inherited to subobjects are:'){#part2 inheritable..
+        break
+    }
+    if($badformat[$i] -match 'Allow (.*)'){
+        $obj.type = 'Allow'
+        
+        
+    }
+
+    else{
+        if($badformat[$i] -match 'Deny (.*)'){
+            $obj.type = 'Deny'
+            
+        }
+        else{
+            continue;
+        }
+    }
+    $maybegroupname = $matches[1]
+    if($maybegroupname -match '(.*?)\s\s+(.*)'){
+        
+        $obj.group = $matches[1]
+        $obj.permissions = ($matches[2] -replace ' {2,}','')+ ';'
+    }
+    else{
+        $obj.group = $maybegroupname
+    }
+    $c = 1;
+    while($badformat[$i+$c][0] -eq ' '){
+        #$temp = $badformat[$i+$c]
+        
+        
+        $obj.permissions = $obj.permissions + ($badformat[$i+$c] -replace ' {2,}','')+ ';'
+        $c++;
+        
+    }
+    $obj
+
+
+    
+}
+
+}
+
